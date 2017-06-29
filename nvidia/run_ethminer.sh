b10e31cd5e3bd2779b7bfd0debef5c8c6227690a
Tim Emiola <adetokunbo@users.noreply.github.com>
2017-06-29 19:10:54 +0900

    Build the latest ethminer
    Start using the ethminer at https://github.com/ethereum-mining/ethminer,
    Genoil's branch at genoil/cpp-ethereum is retired.
    
    Also:
    
    - use new a new feature of the ethminer that improve performance
    cf (http://bit.ly/2skoMC1)

--- nvidia/run_ethminer.sh
+++ nvidia/run_ethminer.sh
@@ -18,31 +18,15 @@ this_dir() {
 
 SCRIPT_DIR=$(this_dir)
 source $(dirname $SCRIPT_DIR)/cfg.sh
-[[ -z $WALLET ]] && {
-    echo "WALLET not defined in the environment"
-    exit 1
-}
-[[ -z $WORKER ]] && {
-    echo "WORKER not defined in the environment"
-    exit 1
-}
-[[ -z $MAIN_POOL ]] && {
-    echo "MAIN_POOL not defined in the environment"
-    exit 1
-}
-[[ -z $FALLBACK_POOL ]] && {
-    echo "FALLBACK_POOL not defined in the environment"
-    exit 1
-}
-MINER_BIN=$HOME/bin/ethminer
+set -u
+echo "Mining to ${WALLET}.${WORKER} at ${MAIN_POOL} || ${FALLBACK_POOL}"
+set +u
 
-set -x
-$MINER_BIN -U \
+$HOME/bin/ethminer -U \
     -S ${MAIN_POOL} \
     -FS ${FALLBACK_POOL}  \
     -O "$WALLET"."$WORKER" \
     --cuda-grid-size 16384 \
     --cuda-block-size 128 \
-    --cuda-streams 4 \
+    --cuda-parallel-hash 8 \
     --farm-recheck 200
-set +x
