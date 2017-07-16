592d4d8c765c2d97eeba6fb3a3d69e1e43d49608
Tim Emiola <adetokunbo@users.noreply.github.com>
2017-07-16 15:50:21 +0900

    Support use of systemd commands in utility functions
    Also
    - update some functions to use systemd rather then invoking scripts directly

--- common/update_bashrc.sh
+++ common/update_bashrc.sh
@@ -6,9 +6,16 @@ add_rig_type() {
     printf "\n\nexport RIG_TYPE=${RIG_TYPE}" >> ~/.bashrc
 }
 
+add_systemctl_rig_type() {
+    printf "\nsystemctl --user import-environment RIG_TYPE\n"  >> ~/.bashrc
+}
+
 update_rig_type() {
     sed --in-place='' -e "s/export RIG_TYPE=.*/export RIG_TYPE=$RIG_TYPE/" ~/.bashrc
 }
 
-grep -q 'export RIG_TYPE' $HOME/.bashrc && update_rig_type || add_rig_type 
+grep -q 'export RIG_TYPE' $HOME/.bashrc && update_rig_type || add_rig_type
+grep -q 'import-environment RIG_TYPE' $HOME/.bashrc || add_systemctl_rig_type
 grep -q $AUTOMINE_RC $HOME/.bashrc || printf "\n\n[ -f ~/$AUTOMINE_RC ] && source ~/${AUTOMINE_RC}\n" >> ~/.bashrc
+cp -v $HOME/bin/automine/common/.screenrc ~/.screenrc
+
