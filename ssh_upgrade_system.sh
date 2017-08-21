6f6e313733c05cb74672cbc23625366e25e93f8d
Tim Emiola <adetokunbo@users.noreply.github.com>
2017-08-21 11:01:38 +0900

    Update to include SSH_PORT if set

--- ssh_upgrade_system.sh
+++ ssh_upgrade_system.sh
@@ -2,4 +2,4 @@
 # Upgrades the system, installing the latest security patches, etc
 
 source rsync_scripts.sh
-ssh -t $SSH_USER \~/bin/automine/common/upgrade_system.sh "$@"
+ssh -t $SSH_USER -p${SSH_PORT} \~/bin/automine/common/upgrade_system.sh "$@"
