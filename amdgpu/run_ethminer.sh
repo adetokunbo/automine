0bb2c0948cbad9f2484fac51a96c51ab5d34cf23
Tim Emiola <adetokunbo@users.noreply.github.com>
2017-08-22 16:33:43 +0900

    Use scan_log with amdgpu mining

--- amdgpu/run_ethminer.sh
+++ amdgpu/run_ethminer.sh
@@ -17,9 +17,23 @@ this_dir() {
 
 SCRIPT_DIR=$(this_dir)
 source $(dirname $SCRIPT_DIR)/cfg.sh
+export AUTOMINE_ALERT_DIR=${AUTOMINE_RUNTIME_DIR}/triggers
+export AUTOMINE_LOG_DIR=${AUTOMINE_RUNTIME_DIR}/logs
 set -u
 echo "Mining to ${WALLET}.${WORKER} at ${MAIN_POOL} || ${FALLBACK_POOL}"
 set +u
+
+# Monitor with simple alerts from grepping the logs.
+#
+# The scan log tool performs a simple grep of known errors and updates files in
+# the alert directory when it detects them. These files are monitored by systemd
+# which takes the appropriate action, e.g, restart the miner.
+SCAN_LOG=$(dirname $SCRIPT_DIR)/common/scan_log.py
+echo "Scanning logs with $SCAN_LOG"
+scan_log() {
+    # drop the port from FALLBACK_POOL, it's not present in the triggering log line
+    FALLBACK_POOL=${FALLBACK_POOL:0:$((${#FALLBACK_POOL}-5))} $SCAN_LOG
+}
 
 export GPU_FORCE_64BIT_PTR=1
 export GPU_USE_SYNC_OBJECTS=1
@@ -27,13 +41,11 @@ export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
 export GPU_MAX_HEAP_SIZE=100
 
-set -x
 $HOME/bin/ethminer \
-    -S asia1.ethermine.org:4444 \
-    -FS us2.ethermine.org:4444 \
+    -S ${MAIN_POOL} \
+    -FS ${FALLBACK_POOL}  \
     -O "$WALLET"."$WORKER" \
     -G \
     --cl-local-work 128 \
     --cl-global-work 16384 \
-    --farm-recheck 200
-set +x
+    --farm-recheck 200  2>&1 | tee >(scan_log)
