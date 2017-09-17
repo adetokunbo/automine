71ca33ff77120b29afb1b27770bf21729e88b5dc
Tim Emiola <adetokunbo@users.noreply.github.com>
2017-09-17 19:31:44 +0900

    Add scripts that provide command dispatch
    - Aim is to allow all automine scripts to be invoked via a single script on the
      host machine
    
    - This is achieved as follows
    
      * add a dispatcher script that runs on the rigs; it invokes commands by name
      and uses logic to determine the location of the actual script corresponding to
      a command's name (cf. dispatch_command.sh)
    
      * add a script for use on the automine host that executes named commands on
      the rigs by executing the dispatcher script via SSH (cf.
      automine_remote_run.sh)
    
    - Also
    
      * ssh_ensure_rig_env can be deleted; the directories it sets up will be
      automatically create by rsync_scripts.sh
    
    Merge me into Add scripts that provide command dispatch

--- rsync_scripts.sh
+++ rsync_scripts.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 source ssh_ensure_env.sh
+
+# Ensure runtime directories are present
 ssh ${SSH_USER} -p${SSH_PORT} mkdir -p \~/bin
+ssh ${SSH_USER} -p${SSH_PORT} mkdir -v -m 777 -p \~/.automine/var/{logs,triggers}
+ssh ${SSH_USER} -p${SSH_PORT} mkdir -v -m 755 -p \~/.automine/{var/src,lib/install}
+
+# Sync the scripts and config
 cp ${AUTOMINE_CFG_PATH} automine_config.json
 rsync -avz -e "ssh -p ${SSH_PORT}" --del --exclude=cfg/* . ${SSH_USER}:~/bin/automine
 rm automine_config.json
 
-# Add config symlinks
+# Add the symlinks used in the commands
 ssh ${SSH_USER} -p${SSH_PORT} ln -sf \~/bin/automine/logging_config.json \~/.automine/var/logs
 ssh ${SSH_USER} -p${SSH_PORT} ln -sf \~/bin/automine/automine_config.json \~/.automine/
 ssh ${SSH_USER} -p${SSH_PORT} ln -sf \~/bin/automine/show_config.py \~/bin/automine_show_config
-
-
+ssh ${SSH_USER} -p${SSH_PORT} ln -sf \~/bin/automine/dispatch_command.sh \~/bin/automine_run
 
