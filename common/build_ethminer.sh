1d509b9832772919fc48dfda36a50702d7d99c47
Tim Emiola <adetokunbo@users.noreply.github.com>
2017-10-24 10:13:41 +0900

    Install cmake if its required
    - also reduce the amount of ethminer that is cloned

--- common/build_ethminer.sh
+++ common/build_ethminer.sh
@@ -1,42 +1,36 @@
 #/bin/bash
 # Build the version of the ethminer for the rig's $RIG_TYPE.
 
-this_dir() {
-    local script_path="${BASH_SOURCE[0]}"
-    if ([ -h "${script_path}" ])
-    then
-        while([ -h "${script_path}" ])
-        do
-            script_path=`readlink "${script_path}"`;
-        done
-    fi
-    pushd . > /dev/null
-    cd $(dirname ${script_path}) > /dev/null
-    script_path=$(pwd);
-    popd  > /dev/null
-    echo $script_path
-}
-
-SCRIPT_DIR=$(this_dir)
 REPO_DIR=$HOME/.automine/var/src
 BIN_DIR=$HOME/bin
 
-ensure_repo_dir() {
+_ensure_repo_dir() {
     [[ -d $REPO_DIR ]] || mkdir -p $REPO_DIR
 }
 
-ensure_bin_dir() {
+_ensure_bin_dir() {
     [[ -d $BIN_DIR ]] || mkdir -p $BIN_DIR
 }
 
-pull_fresh_ethminer() {
+_ensure_cmake() {
+    sudo apt -y update
+    sudo apt -y install cmake
+}
+
+_ensure_environment() {
+    _ensure_repo_dir
+    _ensure_bin_dir
+    _ensure_cmake
+}
+
+_pull_source() {
     cd $REPO_DIR && [[ -d ethminer ]] && rm -fR ethminer
 
     local branch_name=${ETHMINER_BRANCH:=''}
     local github_user=${ETHMINER_GITHUB:='ethereum-mining'}
     local branch_clause=''
     [[ -z $branch_name ]] || branch_clause="-b $branch_name"
-    git clone $branch_clause https://github.com/$github_user/ethminer
+    git clone --depth 1 $branch_clause https://github.com/$github_user/ethminer
 
     local commit=${ETHMINER_COMMIT:=''}
     [[ -z $commit ]] || {
@@ -44,7 +38,7 @@ pull_fresh_ethminer() {
     }
 }
 
-build_ethminer() {
+_build() {
     cd $REPO_DIR/ethminer
     mkdir build
     cd build
@@ -52,25 +46,36 @@ build_ethminer() {
     cmake --build .
 }
 
-cp_to_bin() {
+_install() {
     $BIN_DIR/automine_run minerctl stop || /bin/true
     cp -v $REPO_DIR/ethminer/build/ethminer/ethminer $BIN_DIR/ethminer
     $BIN_DIR/automine_run minerctl restart
 }
 
-add_symlinks() {
-    local rig_dir=$(dirname $SCRIPT_DIR)/$RIG_TYPE
+_pull_source_then_build_and_install() {
+    _pull_source
+    _build
+    _install
+}
+
+_add_symlinks() {
+    local this_dir=$(dirname "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")
+    local rig_dir=$(dirname $this_dir)/$RIG_TYPE
     [ -f ${rig_dir}/add_symlinks.sh ] && source ${rig_dir}/add_symlinks.sh
+}
+
+# update .bashrc with functions that encode typical usage scenarios
+_update_bashrc() {
+    $BIN_DIR/automine_run update_bashrc
+}
+
+_post_install() {
+    _add_symlinks
+    _update_bashrc
 }
 
 set -e
 set -u
-ensure_repo_dir
-ensure_bin_dir
-pull_fresh_ethminer
-build_ethminer
-cp_to_bin
-add_symlinks
-
-# update .bashrc with functions that encode typical usage scenarios
-source $SCRIPT_DIR/update_bashrc.sh
+_ensure_environment
+_pull_source_then_build_and_install
+_post_install
